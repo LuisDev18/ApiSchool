6d49c444212a7a53d5ef60b7ab9f2666c27d7ead
LuisBrian <luisdcm_18@gmail.com>
2023-04-15 18:29:33 -0500

    Configuracion de dependencias

---
+++ src/main/java/com/utp/pe/ApiSchool/ApiSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.utp.pe.ApiSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/pe/ApiSchool/ApiSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.pe.ApiSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
